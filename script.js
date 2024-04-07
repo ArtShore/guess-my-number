1400a166ae280548bb82ea7c4bc558c5ce150af2
Hossein Shouri <mr.h.shouri@gmail.com>
2024-04-07 19:47:09 +0330

    refactored the code dry i modified the input field to have a minimum and a maximum amount, refactored the code clean with new useful functions and deleted loggings that helped me debug

--- script.js
+++ script.js
@@ -1,54 +1,65 @@
 'use strict';
 
-const max = 21;
-const min = 1;
+let randomNum;
 
-let randomNum = Math.floor(Math.random() * (max - min) + min);
+const generateRandomNum = function () {
+  randomNum = Math.floor(Math.random() * (21 - 1) + 1);
+};
+
 let highScore = +document.querySelector('.highscore').textContent;
-console.log(randomNum);
+
+const setBackgroundColor = function (color) {
+  document.body.style.backgroundColor = color;
+};
+
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+
+const disableGame = function (elementClass, condition) {
+  document.querySelector(elementClass).disabled = condition;
+};
+
+generateRandomNum();
 
 document.querySelector('.check').addEventListener('click', function () {
   let userInput = +document.querySelector('.guess').value;
   let score = +document.querySelector('.score').textContent;
-  if (!userInput) {
-    document.querySelector('.message').textContent = '⛔ No Number';
-  } else if (userInput == randomNum) {
-    document.querySelector('.message').textContent = 'Correct! 🎊';
-    document.querySelector('.number').style.width = '30rem';
 
+  if (!userInput) {
+    displayMessage('⛔ No Number');
+  } else if (userInput == randomNum) {
+    displayMessage('Correct! 🎊');
+    document.querySelector('.number').style.width = '30rem';
     document.querySelector('.number').textContent = randomNum;
-    document.body.style.backgroundColor = '#60b347';
+    setBackgroundColor('#60b347');
     score > highScore ? (highScore = score) : 0;
     document.querySelector('.highscore').textContent = highScore;
-    document.querySelector('.guess').setAttribute('disabled', '');
+    disableGame('.guess', true);
+    disableGame('.check', true);
   } else {
-    // document.querySelector('.message').textContent = 'Wrong! ❌';
-    if (userInput > randomNum) {
-      document.querySelector('.message').textContent = 'Go Low 📉';
-    } else if (userInput < randomNum) {
-      document.querySelector('.message').textContent = 'Go High 📈';
-    }
+    displayMessage(userInput > randomNum ? 'Go Lower 📉' : 'Go Higher 📈');
     score -= 1;
     document.querySelector('.score').textContent = score;
     if (score == 0) {
       document.querySelector('.number').style.width = '30rem';
-      document.querySelector('.guess').setAttribute('disabled', '');
-      document.querySelector('.check').setAttribute('disabled', '');
+      disableGame('.guess', true);
+      disableGame('.check', true);
       document.querySelector('.number').textContent = randomNum;
-      document.querySelector('.message').textContent =
-        'You lost you fucking loser! 🤣🤣\n try again!';
-      document.body.style.backgroundColor = '#8b0000';
+      displayMessage('You lost you fucking loser! 🤣🤣\n try again!');
+      setBackgroundColor('#8b0000');
     }
   }
 });
+
 document.querySelector('.again').addEventListener('click', function () {
   document.querySelector('.number').style.width = '15rem';
-  randomNum = Math.floor(Math.random() * (max - min) + min);
-  document.body.style.backgroundColor = '#222';
-  document.querySelector('.guess').disabled = false;
-  document.querySelector('.check').disabled = false;
+  generateRandomNum();
+  setBackgroundColor('#222');
+  disableGame('.guess', false);
+  disableGame('.check', false);
   document.querySelector('.guess').value = '';
-  document.querySelector('.message').textContent = 'Start guessing...';
+  displayMessage('Start guessing...');
   document.querySelector('.number').textContent = '?';
   document.querySelector('.score').textContent = '20';
 });
